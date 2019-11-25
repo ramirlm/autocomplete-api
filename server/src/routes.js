5353125a3ec8dc4712d40dcfbb88948d254983c3
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-11-25 20:14:31 -0300

    Fixing query param into querying route

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,7 @@ import BookController from './app/controllers/BookController';
 
 const routes = new Router();
 
-routes.get('/books/:qry', BookController.query);
+routes.get('/books', BookController.query);
 routes.post('/books', BookController.store);
 
 export default routes;
