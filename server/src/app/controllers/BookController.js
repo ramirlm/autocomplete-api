2a064b3ae0415443e594bb1e284a1cc056e9cd93
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-11-25 21:38:10 -0300

    Test green for multiple query book params search

--- server/src/app/controllers/BookController.js
+++ server/src/app/controllers/BookController.js
@@ -12,15 +12,20 @@ class BookController {
   }
 
   async query(req, res) {
-    const query = req.query.q.toLowerCase();
+    const query = req.query.q;
+    const queryNum = isNaN(parseInt(query, 0)) ? 0 : parseInt(query, 0);
 
-    const books = await book.findAll({
-      where: sequelize.where(
-        sequelize.fn('LOWER', sequelize.col('title')),
-        'LIKE',
-        `%${query}%`
-      ),
-    });
+    const books = await book.sequelize.query(
+      'SELECT "title" from "books" WHERE LOWER("title") like (:query) OR LOWER("author") like (:query) OR "year" = (:queryNum)',
+      {
+        replacements: {
+          query: `%${query.toLowerCase()}%`,
+          queryNum,
+        },
+        model: book,
+        mapToModel: true,
+      }
+    );
     return res.status(200).json(books);
   }
 }
