935464acf291d745a91ca8ddf62eb95b295c7c01
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-11-26 06:50:48 -0300

    Fixing controller and tests

--- server/src/app/controllers/BookController.js
+++ server/src/app/controllers/BookController.js
@@ -12,20 +12,20 @@ class BookController {
 
   async query(req, res) {
     const query = req.query.q;
-    const queryNum = isNaN(parseInt(query, 0)) ? 0 : parseInt(query, 0);
+    const queryNumber = isNaN(parseInt(query, 0)) ? 0 : parseInt(query, 0);
 
     const books = await book.sequelize.query(
-      'SELECT "title" from "books" WHERE LOWER("title") like (:query) OR LOWER("author") like (:query) OR "year" = (:queryNum)',
+      'SELECT * from "books" WHERE LOWER("title") like (:query) OR LOWER("author") like (:query) OR "year" = (:queryNumber) LIMIT 10',
       {
         replacements: {
           query: `%${query.toLowerCase()}%`,
-          queryNum,
+          queryNumber,
         },
         model: book,
         mapToModel: true,
       }
     );
-    return res.status(200).json(books);
+    return res.status(200).json({ books });
   }
 }
 
